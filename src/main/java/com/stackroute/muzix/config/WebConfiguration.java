eeeaadaf6400c24d79ab88ef9457851bf9189895
Neelanshi <neelanshibhatnagar@ymail.com>
2019-07-24 21:19:10 +0530

    sixth task

--- src/main/java/com/stackroute/muzix/config/WebConfiguration.java
+++ src/main/java/com/stackroute/muzix/config/WebConfiguration.java
@@ -1,14 +1,47 @@
 package com.stackroute.muzix.config;
 //
+import com.stackroute.muzix.domain.Track;
+import com.stackroute.muzix.exception.TrackAlreadyExistsException;
 import com.stackroute.muzix.repository.TrackRepository;
+import com.stackroute.muzix.service.TrackService;
 import org.apache.catalina.servlets.WebdavServlet;
 import org.h2.server.web.WebServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration {
+
+    private final
+    TrackService trackService;
+
+    @Autowired
+    public WebConfiguration(TrackService trackService) {
+        this.trackService = trackService;
+    }
+
+    @EventListener
+    public void handleContextRefreshEvent(ContextRefreshedEvent cfr) {
+        try {
+            trackService.saveTrack(new Track(1,"Summer","Calvin Harris"));
+            trackService.saveTrack(new Track(2,"Love me like","Olivia"));
+            System.out.println("Context Refreshed");
+        } catch (TrackAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     @Bean
     ServletRegistrationBean h2servletRegistration(){
@@ -16,6 +49,15 @@ public class WebConfiguration {
         servletRegistrationBean.addUrlMappings("/console/*");
         return servletRegistrationBean;
 
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.stackroute.muzix.controllers"))
+                .paths(PathSelectors.regex("/.*"))
+                .build();
     }
 
 
