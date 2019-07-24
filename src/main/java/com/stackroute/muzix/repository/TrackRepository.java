e359805649f15648e0af029272a54b6e98f06b47
Neelanshi <32795046+DNeelanshi@users.noreply.github.com>
2019-07-24 15:02:06 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/muzix/repository/TrackRepository.java
+++ src/main/java/com/stackroute/muzix/repository/TrackRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface TrackRepository extends JpaRepository<Track,Integer> {
 
-    @Query(value = "SELECT * FROM TRACK t WHERE t.name = ?1")
-    public List<Track> getTrackbyName(String name);
+    @Query("SELECT t FROM Track t WHERE t.name = ?1")
+   List<Track> getTrackbyName(String name);
 }
