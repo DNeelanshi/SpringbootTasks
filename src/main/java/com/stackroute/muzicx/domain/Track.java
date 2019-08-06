44325ce5003963aff85eed9c35523484aae97897
Neelanshi <32795046+DNeelanshi@users.noreply.github.com>
2019-08-07 01:20:47 +0530

    Update Track.java

--- src/main/java/com/stackroute/muzicx/domain/Track.java
+++ src/main/java/com/stackroute/muzicx/domain/Track.java
@@ -1,10 +1,18 @@
 package com.stackroute.muzicx.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
 
     @Id
@@ -12,44 +20,13 @@ public class Track {
     private int id;
 
     @Column
-
     private String name;
 
     @Column
     private String comment;
 
-    public int getId() {
-        return id;
-    }
+}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 
     public Track(int id, String name, String comment) {
         this.id = id;
