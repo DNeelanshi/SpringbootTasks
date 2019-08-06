0c8ec5ecd5cc8867ee9e186208e420b72a9db5b5
Neelanshi <32795046+DNeelanshi@users.noreply.github.com>
2019-08-06 22:55:42 +0530

    Update TrackService.java

--- src/main/java/com/stackroute/muzix/service/TrackService.java
+++ src/main/java/com/stackroute/muzix/service/TrackService.java
@@ -6,13 +6,11 @@ import java.util.List;
 
 public interface TrackService {
 
-    public Track saveTrack(Track track);
+     Track saveTrack(Track track);
 
-    public List<Track>getAllTracks();
+     List<Track>getAllTracks();
 
-    public int deleteTrack(long id);
+     int deleteTrack(long id);
 
-    public Track getTrackById(int noteId);
-
-//    public Track UpdateTrack(Track track);
+     Track getTrackById(int noteId);
 }
